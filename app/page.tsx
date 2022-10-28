0bd6c43239b4aced51060053de02246ec09d80e0
Sahil Deshmukh <sahildmk@gmail.com>
2022-10-28 18:49:06 +1100

    upgrade to NextJS 13

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Splitty</title>
+        <meta name="description" content="Split things" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col justify-center items-center h-screen">
+        <h1 className="text-6xl font-bold pb-14">Splitty</h1>
+        <Link href={"/transactions"}>
+          <div className="text-xl font-bold hover:cursor-pointer hover:scale-105 transition-all dark:text-slate-700 dark:bg-slate-200 px-5 py-3 rounded-md">
+            View Transactions
+          </div>
+        </Link>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
